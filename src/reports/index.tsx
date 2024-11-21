3cb1f25adb0c98d640f9dfa3cb34511ca1968ff4
titi08titi <48367299+titi08titi@users.noreply.github.com>
2024-11-21 23:42:35 +0100

    Delete src/reports directory

--- src/reports/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as ReportsCogs } from './cogs/reportsCogs'
-export { default as ManageIntrastat } from './manageIntrastat/manageIntrastat'
-export { default as ReportsIntrastat } from './intrastat/reportsIntrastat'
-export { default as ReportsPlasticTax } from './plasticTax/reportsPlasticTax'
-export { default as ReportsSynthesis } from './synthesis/reportsSynthesis'
